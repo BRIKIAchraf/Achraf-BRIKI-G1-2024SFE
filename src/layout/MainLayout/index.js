e5fb579979d02bf8a0a2028f601ee1637fe54656
BRIKIAchraf <brikiachraf@DJERBA.r-iset.tn>
2024-05-21 16:27:16 +0100

    -Refactor:remove the lamp and make the message is the indecator

--- src/layout/MainLayout/index.js
+++ src/layout/MainLayout/index.js
@@ -4,8 +4,6 @@ import { Outlet } from 'react-router-dom';
 // material-ui
 import { styled, useTheme } from '@mui/material/styles';
 import { useMediaQuery, AppBar, Box, Toolbar } from '@mui/material';
-import { CircularProgress } from '@mui/material';
-import { Brightness7 as LampOnIcon, Brightness4 as LampOffIcon } from '@mui/icons-material';
 import { keyframes } from '@mui/material/styles';
 
 // project import
@@ -32,32 +30,6 @@ const OutletDiv = styled((props) => <div {...props} />)(({ theme }) => ({
   position: 'relative'
 }));
 
-const LED = styled((props) => <div {...props} />)(({ theme, blinking }) => ({
-  width: '30px',
-  height: '30px',
-  borderRadius: '50%',
-  position: 'absolute',
-  top: '10%',
-  right: '16px',
-  transform: 'translateY(-50%)',
-  animation: blinking ? `${blink} 1s infinite alternate` : 'none',
-}));
-
-const Message = styled((props) => <div {...props} />)(({ theme }) => ({
-  position: 'absolute',
-  top: '0',
-  left: '50%',
-  transform: 'translate(-50%, 0)',
-  color: 'white',
-  fontSize: '16px',
-  fontWeight: 'bold',
-  textAlign: 'center',
-  backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  padding: '10px',
-  borderRadius: '5px',
-  zIndex: -1, // Set z-index to -1 to display behind LED
-}));
-
 const blink = keyframes`
   from {
     opacity: 1;
@@ -68,6 +40,21 @@ const blink = keyframes`
   }
 `;
 
+const Message = styled((props) => <div {...props} />)(({ theme, color }) => ({
+  position: 'absolute',
+  top: '10%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  color: 'white',
+  fontSize: '16px',
+  fontWeight: 'bold',
+  textAlign: 'center',
+  backgroundColor: color,
+  padding: '10px',
+  borderRadius: '5px',
+  animation: `${blink} 1s infinite alternate`,
+}));
+
 // ==============================|| MAIN LAYOUT ||============================== //
 
 const MainLayout = () => {
@@ -75,7 +62,6 @@ const MainLayout = () => {
   const matchUpMd = useMediaQuery(theme.breakpoints.up('md'));
   const [drawerOpen, setDrawerOpen] = useState(false);
   const [apiStatus, setApiStatus] = useState('');
-  const [blinking, setBlinking] = useState(false);
 
   const handleDrawerToggle = () => {
     setDrawerOpen(!drawerOpen);
@@ -100,16 +86,6 @@ const MainLayout = () => {
     fetchApiStatus();
   }, []);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setBlinking((prevBlinking) => !prevBlinking);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  const ledColor = apiStatus === 'en panne' ? 'red' : 'green';
-
   return (
     <Box sx={{ display: 'flex', width: '100%' }}>
       <AppBar position="fixed" sx={{ zIndex: 1200 }}>
@@ -127,8 +103,11 @@ const MainLayout = () => {
         <Box sx={theme.mixins.toolbar} />
         <OutletDiv>
           <Outlet />
-          <LED style={{ backgroundColor: ledColor }} blinking={blinking} />
-          <Message>{apiStatus === 'en panne' && 'Le système est en panne'}</Message>
+          {apiStatus === 'en panne' ? (
+            <Message color="rgba(255, 0, 0, 0.7)">Le système est en panne</Message>
+          ) : (
+            <Message color="rgba(0, 128, 0, 0.7)">Le système fonctionne correctement</Message>
+          )}
         </OutletDiv>
       </Main>
     </Box>
